aace62ef399cc40be96a52ad87211073d1f0da96
root <root@MH320-78120.avc.edu>
2026-03-09 13:40:20 -0700

    A31 Done However, still having troubles getting data/run.sh to run on the lab PC.

--- main.cpp
+++ main.cpp
@@ -8,19 +8,24 @@ int main()
   const double ClassCCost = 9.00;
 
   // TODO: Declare integer variables for tickets sold for each class
-
+  int ticketsA, ticketsB, ticketsC;
   // TODO: Declare double variables for total sales per class and grand total
-
+  double totalA, totalB, totalC, total;
   // TODO: Prompt user and read number of Class A tickets sold
-
+  cout << "Insert number of Class A tickets" << endl;
+  cin >> ticketsA;
   // TODO: Prompt user and read number of Class B tickets sold
-
+  cout << "Insert number of Class B tickets" << endl;
+  cin >> ticketsB;
   // TODO: Prompt user and read number of Class C tickets sold
-
+  cout << "Insert number of Class C tickets" << endl;
+  cin >> ticketsC;
   // TODO: Calculate sales for each class
-
+  totalA = ticketsA*ClassACost;
+  totalB = ticketsB*ClassBCost;
+  totalC = ticketsC*ClassCCost;
   // TODO: Calculate total sales
-
+  total = totalA+totalB+totalC;
   cout << setprecision(2) << fixed;
   cout << "Tickets sold for Class A " << ticketsA << " Sales Price for A   " << totalA << endl;
   cout << "Tickets sold for Class B " << ticketsB << " Sales Price for B   " << totalB << endl;
